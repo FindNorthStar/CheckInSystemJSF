ba5a91b42b7fa55c91f2fc394347763ba6296fbf
Alexander Qi <xdqi@users.noreply.github.com>
2016-07-04 09:00:25 +0800

    Add checkin page

---
+++ src/main/java/edu/bupt/checkinsystem/CheckIn.java
@@ -0,0 +1,12 @@
+package edu.bupt.checkinsystem;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+
+@ManagedBean(name="checkin")
+@SessionScoped // TODO:
+public class CheckIn implements Serializable {
+
+}
