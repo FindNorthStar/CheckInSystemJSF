a48361c3dc05c9c7ca03dfbc4dd7fa28a630c06a
Alexander Qi <xdqi@users.noreply.github.com>
2016-07-04 08:49:32 +0800

    Initial commit

---
+++ src/main/java/edu/bupt/checkinsystem/Register.java
@@ -0,0 +1,24 @@
+package edu.bupt.checkinsystem;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+
+@ManagedBean(name="register")
+@SessionScoped // TODO:
+public class Register implements Serializable{
+
+    private static final long serialVersionUID = -6913972022251814607L;
+
+    private String s1 = "Hello World!!";
+
+    public String getS1() {
+        return s1;
+    }
+
+    public void setS1(String s1) {
+        this.s1 = s1;
+    }
+
+}
