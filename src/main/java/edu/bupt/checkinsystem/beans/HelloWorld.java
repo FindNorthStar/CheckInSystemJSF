16cea79a3963dfca576d34f6aea325a5aec4e5e7
Wafer Li <omyshokami@gmail.com>
2016-07-04 22:08:47 +0900

    WIP: Test Entity

--- src/main/java/edu/bupt/checkinsystem/beans/HelloWorld.java
+++ src/main/java/edu/bupt/checkinsystem/beans/HelloWorld.java
@@ -1,8 +1,12 @@
 package edu.bupt.checkinsystem.beans;
 
-import java.io.Serializable;
+import edu.bupt.checkinsystem.entities.TypeEntity;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import java.io.Serializable;
 
 
 @ManagedBean(name="helloWorld")
@@ -14,7 +18,12 @@ public class HelloWorld implements Serializable{
     private String s1 = "Hello World!!";
 
     public String getS1() {
-        System.out.println(s1);
+        EntityManager entityManager = Persistence.createEntityManagerFactory("NewPersistenceUnit").createEntityManager();
+        entityManager.getTransaction().begin();
+        TypeEntity typeEntity = new TypeEntity();
+        typeEntity.setName("name");
+        entityManager.persist(typeEntity);
+        entityManager.getTransaction().commit();
         return s1;
     }
 
