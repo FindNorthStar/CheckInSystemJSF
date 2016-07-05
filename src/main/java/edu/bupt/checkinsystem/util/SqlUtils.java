ebc9df653359e4140b399af866995e33efaf2189
Alexander Qi <xdqi@users.noreply.github.com>
2016-07-05 15:00:05 +0800

    Add prepared select support

--- src/main/java/edu/bupt/checkinsystem/util/SqlUtils.java
+++ src/main/java/edu/bupt/checkinsystem/util/SqlUtils.java
@@ -27,6 +27,30 @@ public class SqlUtils {
         List<Map<String, Object>> rows = new ArrayList<Map<String, Object>>();
         getHashMap(rows, resultSet);
 
+        preparedStatement.close();
+        connection.close();
+        return rows;
+    }
+
+    /**
+     * 执行 SQL 类 SELECT 型查询
+     * @param query SQL 语句
+     * @param args 查询参数, 第一个参数从 1 开始
+     * @return 数据列
+     * @throws Exception
+     */
+    public static List<Map<String, Object>> executeSqlQuery(String query, Map<Integer, Object> args) throws Exception {
+        Connection connection = Config.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        for (Map.Entry<Integer, Object> column : args.entrySet()) {
+            preparedStatement.setObject(column.getKey(), column.getValue());
+        }
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        List<Map<String, Object>> rows = new ArrayList<Map<String, Object>>();
+        getHashMap(rows, resultSet);
+
+        preparedStatement.close();
         connection.close();
         return rows;
     }
@@ -34,11 +58,11 @@ public class SqlUtils {
     /**
      * 执行单行插入/更新
      * @param query SQL 语句
-     * @param row 存数据的行
+     * @param row 存数据的行, 第一个参数从 1 开始
      * @return 状态, 见 https://docs.oracle.com/javase/7/docs/api/java/sql/Statement.html#SUCCESS_NO_INFO
      * @throws Exception
      */
-    public static int executeSqlQuery(String query, Map<Integer, Object> row) throws Exception {
+    public static int executeSqlUpdate(String query, Map<Integer, Object> row) throws Exception {
         Connection connection = Config.getConnection();
         connection.setAutoCommit(false);
         PreparedStatement preparedStatement = connection.prepareStatement(query);
@@ -48,6 +72,7 @@ public class SqlUtils {
         preparedStatement.addBatch();
         int[] batch = preparedStatement.executeBatch();
 
+        preparedStatement.close();
         connection.close();
         return batch[0];
     }
@@ -55,11 +80,11 @@ public class SqlUtils {
     /**
      * 执行多行插入/更新
      * @param query SQL 语句
-     * @param rows 存数据的行
+     * @param rows 存数据的行, Map 内第一个参数从 1 开始
      * @return 状态, 与 List 顺序相同。见 https://docs.oracle.com/javase/7/docs/api/java/sql/Statement.html#SUCCESS_NO_INFO
      * @throws Exception
      */
-    public static int[] executeSqlQuery(String query, List<Map<Integer, Object>> rows) throws Exception {
+    public static int[] executeSqlUpdate(String query, List<Map<Integer, Object>> rows) throws Exception {
         Connection connection = Config.getConnection();
         connection.setAutoCommit(false);
         PreparedStatement preparedStatement = connection.prepareStatement(query);
@@ -71,6 +96,7 @@ public class SqlUtils {
         }
         int[] batchResult = preparedStatement.executeBatch();
 
+        preparedStatement.close();
         connection.close();
         return batchResult;
     }
