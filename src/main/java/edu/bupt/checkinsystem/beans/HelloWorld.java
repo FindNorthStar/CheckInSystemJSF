6815188cadbebb991579e6b39540e1f4291d29f0
Alexander Qi <xdqi@users.noreply.github.com>
2016-07-05 09:46:24 +0800

    Use JDBC directly

--- src/main/java/edu/bupt/checkinsystem/beans/HelloWorld.java
+++ src/main/java/edu/bupt/checkinsystem/beans/HelloWorld.java
@@ -4,10 +4,8 @@ import edu.bupt.checkinsystem.entities.TypeEntity;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
 import java.io.Serializable;
-
+import java.sql.*;
 
 @ManagedBean(name="helloWorld")
 @SessionScoped
@@ -17,14 +15,28 @@ public class HelloWorld implements Serializable{
 
     private String s1 = "Hello World!!";
 
-    public String getS1() {
-        EntityManager entityManager = Persistence.createEntityManagerFactory("NewPersistenceUnit").createEntityManager();
-        entityManager.getTransaction().begin();
-        TypeEntity typeEntity = new TypeEntity();
-        typeEntity.setName("name");
-        entityManager.persist(typeEntity);
-        entityManager.getTransaction().commit();
-        return s1;
+    private Connection newConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://10.125.103.123:3306/sunyi", "sunyi", "sunyi");
+    }
+
+    public String getS1() throws Exception {
+        try {
+            Connection conn = this.newConnection();
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM type");
+            ResultSet result = ps.executeQuery();
+            StringBuilder sb = new StringBuilder();
+            while (result.next()) {
+                sb.append(String.valueOf(result.getInt("id")));
+                sb.append(" ");
+                sb.append(result.getString("name"));
+                sb.append("\n");
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            String a = "fuck";
+            throw e;
+        }
     }
 
     public void setS1(String s1) {
