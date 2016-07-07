9d35249e4916499c330d3b4e325f6b459d4a212f
Alexander Qi <xdqi@users.noreply.github.com>
2016-07-07 15:38:22 +0800

    Fill backend courses with content

---
+++ src/main/java/edu/bupt/checkinsystem/beans/backend/Courses.java
@@ -0,0 +1,36 @@
+package edu.bupt.checkinsystem.beans.backend;
+
+import com.sun.javadoc.SerialFieldTag;
+import edu.bupt.checkinsystem.util.SqlUtils;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+@ManagedBean(name = "backend_courses")
+@RequestScoped
+public class Courses implements Serializable {
+    private List<Map<String, Object>> records = null;
+    private static final String listAllCoursesSql =
+            "SELECT course.id, " +
+                    "course.courseName, " +
+                    "GROUP_CONCAT(class.classNo SEPARATOR ', ') AS classNumbers, " +
+                    "course.teachers\n" +
+            "FROM\n" +
+            "    class, course, courseClass\n" +
+            "WHERE\n" +
+            "    class.id = courseClass.classId AND\n" +
+            "    courseClass.courseId = course.id\n" +
+            "GROUP BY\n" +
+            "    course.courseName;";
+
+
+    public List<Map<String, Object>> getRecords() throws Exception {
+        if (records == null) {
+            records = SqlUtils.executeSqlQuery(listAllCoursesSql);
+        }
+        return records;
+    }
+}
