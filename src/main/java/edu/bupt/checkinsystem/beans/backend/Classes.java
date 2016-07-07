8ae3c47268345f9cea7210b5b5d9d1ee91a651ca
Yiming Wang <wangym5106@gmail.com>
2016-07-07 15:04:38 +0800

    Finish class page

---
+++ src/main/java/edu/bupt/checkinsystem/beans/backend/Classes.java
@@ -0,0 +1,24 @@
+package edu.bupt.checkinsystem.beans.backend;
+
+import edu.bupt.checkinsystem.Globals;
+import edu.bupt.checkinsystem.util.NetUtils;
+import edu.bupt.checkinsystem.util.SqlUtils;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ManagedBean(name = "classes")
+@RequestScoped
+public class Classes implements Serializable {
+    private List<Map<String, Object>> classes = null;
+    public List<Map<String, Object>> getClasses() throws Exception {
+        classes = SqlUtils.executeSqlQuery("SELECT classNo FROM class");
+        return classes;
+    }
+}
