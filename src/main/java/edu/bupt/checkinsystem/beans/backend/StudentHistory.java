cbe18a9e58a358678261fe7bce69d5db1d06ccef
Yiming Wang <wangym5106@gmail.com>
2016-07-08 15:01:35 +0800

    Fix a bug when startDate or endDate is empty

--- src/main/java/edu/bupt/checkinsystem/beans/backend/StudentHistory.java
+++ src/main/java/edu/bupt/checkinsystem/beans/backend/StudentHistory.java
@@ -46,14 +46,14 @@ public class StudentHistory implements Serializable {
 
     public String getStartDate() {
         startDate = Faces.getRequestParameter("start");
-        if (startDate == null)
+        if (startDate == null || startDate == "")
             startDate = "1970-01-01";
         return startDate;
     }
 
     public String getEndDate() {
         endDate = Faces.getRequestParameter("end");
-        if (endDate == null) {
+        if (endDate == null || endDate == "") {
             endDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
         }
         return endDate;
