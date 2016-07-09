3cc67b1bbe4e71c5b6a0aae265ed90d0eac0e203
Alexander Qi <xdqi@users.noreply.github.com>
2016-07-09 15:47:48 +0800

    Fill date when URL contains date

---
+++ src/main/webapp/resources/js/jquery-urlparam.js
@@ -0,0 +1,14 @@
+/**
+ * jQuery URLParam plugin
+ * Copyright bjverde
+ */
+
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null){
+        return null;
+    }
+    else{
+        return results[1] || 0;
+    }
+};
